309eec302715085022eff1afdaedf27e9fb72dff
nowickazuzanna <zuzannanowicka17@gmail.com>
2023-06-02 12:49:48 +0200

    proba

--- login.php
+++ login.php
@@ -1,3 +1,4 @@
+
 <?php
 session_start();
 ?>
@@ -7,10 +8,17 @@ session_start();
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Logowanie</title>
-
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="../plugins/fontawesome-free/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="../dist/css/adminlte.min.css">
 </head>
-<body class="hold-transition login-page">
-    <?php
+<body class="bg-dark hold-transition login-page text-light">
+<?php
 if (isset($_SESSION["error"])) {
     echo <<< ERROR
        <div class="alert alert-danger alert-dismissible"> 
@@ -32,52 +40,40 @@ if (isset($_SESSION["success"])) {
     ERROR;
     unset($_SESSION["success"]);
 }
+?>
 
-    ?>
-<div class="card card-outline card-primary">
-    <?php
-    require_once "header.php";
+<div>
+    <?php 
+    require_once "header.php"; 
     $redirect = isset($_GET["redirect"]) ? $_GET["redirect"] : "../index.php";
     ?>
     <div class="card-body">
-        <h4> Zaloguj sięeeeeeeee </h4>
+        <form action="scripts/register.php" method="post" class="container p-3">
+            <h3 class="login-box-msg mb-4">Logowanie</h3>
 
-        <!-- czy link do przekierowania może być jako parametr w linku? -->
+            <!-- czy link do przekierowania może być jako parametr w linku? -->
         <form action="./scripts/login.php?redirect=<?php echo $redirect; ?>" method="post">
 
-            <div class="input-group mb-3">
-                <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
-                <div class="col-sm-10">
-                    <input type="email" class="form-control" placeholder="Podaj email" name="email">
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fas fa-envelope"></span>
-                        </div>
-                    </div>
-                </div>
+            <div class="my-3">
+                <label for="email1" class="form-label">Email</label>
+                <input type="email" class="form-control" placeholder="Podaj email" name="email1" id="email1">
             </div>
 
-            <div class="input-group mb-3">
-                <label for="inputPassword3" class="col-sm-2 control-label">Hasło</label>
-                <div class="col-sm-10">
-                    <input type="password" class="form-control" placeholder="Podaj hasło" name="pass">
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fas fa-lock"></span>
-                        </div>
-                    </div>
-                </div>
+            <div class="my-3">
+                <label for="pass1" class="form-label">Hasło</label>
+                <input type="password" class="form-control" placeholder="Podaj hasło" name="pass1" id="pass1">
             </div>
 
-            <div class="col-7">
-                <button type="submit" class="btn btn-primary btn-block">Zaloguj</button>
+            <div class="col-5">
+        <p> Nie masz jeszcze konta? <a href="register.php" class="text-center">Zarejestruj się</a></p>
             </div>
         </form>
     </div>
-
-    <div class="col-5">
-        <p> Nie masz jeszcze konta? <a href="register.php" class="text-center">Zarejestruj się</a></p>
-    </div>
 </div>
+<!-- jQuery -->
+<script src="../plugins/jquery/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+<!-- AdminLTE App -->
+<script src="../dist/js/adminlte.min.js"></script>
 </body>
 </html>
