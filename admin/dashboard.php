02ee20d633b9a038d3c7f9560927eba6b7efb1b6
malinuuus <maliny.brzoskwiniowe@gmail.com>
2023-06-04 14:25:02 +0200

    admin lte

---
+++ admin/dashboard.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AdminLTE 3 | Dashboard 2</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome Icons -->
+    <link rel="stylesheet" href="../AdminLTE-3.2.0/plugins/fontawesome-free/css/all.min.css">
+    <!-- overlayScrollbars -->
+    <link rel="stylesheet" href="../AdminLTE-3.2.0/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="../AdminLTE-3.2.0/dist/css/adminlte.min.css">
+</head>
+<body class="hold-transition dark-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+<div class="wrapper">
+
+    <!-- Navbar -->
+    <?php
+    require_once "views/navbar.php";
+    ?>
+    <!-- /.navbar -->
+
+    <!-- Main Sidebar Container -->
+    <?php
+    require_once "views/logged_admin/aside.php";
+    ?>
+
+    <!-- Content Wrapper. Contains page content -->
+    <?php
+    require_once "views/logged_admin/content.php";
+    ?>
+    <!-- /.content-wrapper -->
+
+    <!-- Main Footer -->
+    <?php
+    require_once "views/footer.php";
+    ?>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+<!-- jQuery -->
+<script src="../AdminLTE-3.2.0/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap -->
+<script src="../AdminLTE-3.2.0/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="../AdminLTE-3.2.0/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE App -->
+<script src="../AdminLTE-3.2.0/dist/js/adminlte.js"></script>
+
+<!-- PAGE ../AdminLTE-3.2.0/plugins -->
+<!-- jQuery Mapael -->
+<script src="../AdminLTE-3.2.0/plugins/jquery-mousewheel/jquery.mousewheel.js"></script>
+<script src="../AdminLTE-3.2.0/plugins/raphael/raphael.min.js"></script>
+<script src="../AdminLTE-3.2.0/plugins/jquery-mapael/jquery.mapael.min.js"></script>
+<script src="../AdminLTE-3.2.0/plugins/jquery-mapael/maps/usa_states.min.js"></script>
+<!-- ChartJS -->
+<script src="../AdminLTE-3.2.0/plugins/chart.js/Chart.min.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="../AdminLTE-3.2.0/dist/js/pages/dashboard2.js"></script>
+</body>
+</html>
